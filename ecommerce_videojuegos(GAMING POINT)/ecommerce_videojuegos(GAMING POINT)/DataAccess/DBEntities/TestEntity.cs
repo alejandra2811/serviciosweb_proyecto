76bcdc398859e7e04a3685941d2f2aaa113311dc
Bruno <brunosat2003@gmail.com>
2023-11-17 21:11:06 -0500

    Test

---
+++ ecommerce_videojuegos(GAMING POINT)/ecommerce_videojuegos(GAMING POINT)/DataAccess/DBEntities/TestEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace ecommerce_videojuegos_GAMING_POINT_.DataAccess.DBEntities
+{
+    public class TestEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
