2ef24e7869b1e050728b85ce08d76c60f83ed12f
Alejandra Bolaños <alejandrabolanos2811@gmail.com>
2023-11-17 20:42:58 -0500

    first commit

---
+++ ecommerce_videojuegos(GAMING POINT)/ecommerce_videojuegos(GAMING POINT)/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ecommerce_videojuegos_GAMING_POINT_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
